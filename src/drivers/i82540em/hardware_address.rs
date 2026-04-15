aeb5b11f38af0229ed62772f3315674f65fc643a
Emrio <commits@emrio.fr>
2026-04-15 14:59:07 +0200

    feat: hw addr add tests

--- src/drivers/i82540em/hardware_address.rs
+++ src/drivers/i82540em/hardware_address.rs
@@ -46,3 +46,56 @@ impl core::fmt::Display for HardwareAddress {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use core::fmt::Write;
+
+    use super::HardwareAddress;
+
+    // FIXME: string allocation
+    struct StringBuffer<const N: usize> {
+        buf: [u8; N],
+        pos: usize,
+    }
+
+    impl<const N: usize> StringBuffer<N> {
+        fn new() -> Self {
+            Self {
+                buf: [0u8; N],
+                pos: 0,
+            }
+        }
+
+        fn as_str(&self) -> &str {
+            core::str::from_utf8(&self.buf[..self.pos]).unwrap()
+        }
+    }
+
+    impl<const N: usize> core::fmt::Write for StringBuffer<N> {
+        fn write_str(&mut self, s: &str) -> core::fmt::Result {
+            let bytes = s.as_bytes();
+            self.buf[self.pos..self.pos + bytes.len()].copy_from_slice(bytes);
+            self.pos += bytes.len();
+            Ok(())
+        }
+    }
+
+    #[test_case]
+    fn test_hw_addr_formatter() {
+        let addr = HardwareAddress::from(0x1234, 0x5678, 0x9abc);
+
+        let mut buffer = StringBuffer::<17>::new();
+        write!(buffer, "{}", addr).unwrap();
+        assert_eq!(buffer.as_str(), "34:12:78:56:bc:9a");
+    }
+
+    #[test_case]
+    fn test_hw_addr_low_digits() {
+        let addr = HardwareAddress::from(0x0102, 0x120a, 0x0fff);
+
+        let mut buffer = StringBuffer::<17>::new();
+        write!(buffer, "{}", addr).unwrap();
+        assert_eq!(buffer.as_str(), "02:01:0a:12:ff:0f");
+    }
+}
