934ae1221ad656e72baac19c2278698e98e78cc5
Emrio <commits@emrio.fr>
2026-08-04 00:09:04 +0200

    feat(async): handle state machine updates without interrupts

--- src/task/executor.rs
+++ src/task/executor.rs
@@ -3,28 +3,59 @@ extern crate alloc;
 use alloc::sync::Arc;
 use alloc::task::Wake;
 use core::pin::pin;
-use core::task::{Context, Poll};
+use core::sync::atomic::{AtomicBool, Ordering};
+use core::task::{Context, Poll, Waker};
 use x86_64::instructions::interrupts;
 
-struct Waker;
+/**
+ * This waker implementation works fine while all futures rely:
+ * - on interrupts (un-hlt the thread);
+ * - or update internal logic (caught by the atomic boolean).
+ */
+struct MyWaker {
+    notified: AtomicBool,
+}
 
-impl Wake for Waker {
-    fn wake(self: Arc<Self>) {}
-    fn wake_by_ref(self: &Arc<Self>) {}
+impl MyWaker {
+    fn new() -> Self {
+        Self {
+            notified: AtomicBool::new(false),
+        }
+    }
+
+    fn notify(&self) {
+        self.notified.store(true, Ordering::Release);
+    }
+}
+
+impl Wake for MyWaker {
+    fn wake(self: Arc<Self>) {
+        self.notify();
+    }
+
+    fn wake_by_ref(self: &Arc<Self>) {
+        self.notify();
+    }
 }
 
 pub fn block_on<T>(future: impl Future<Output = T>) -> T {
-    let waker = core::task::Waker::from(Arc::new(Waker));
+    let mywaker = Arc::new(MyWaker::new());
+    let waker = Waker::from(mywaker.clone());
     let mut cx = Context::from_waker(&waker);
     let mut future = pin!(future);
     loop {
         interrupts::disable();
-        match future.as_mut().poll(&mut cx) {
-            Poll::Ready(result) => {
-                interrupts::enable();
-                return result;
-            }
-            Poll::Pending => interrupts::enable_and_hlt(),
+        mywaker.notified.store(false, Ordering::Relaxed);
+
+        if let Poll::Ready(result) = future.as_mut().poll(&mut cx) {
+            interrupts::enable();
+            return result;
+        }
+
+        if !mywaker.notified.load(Ordering::Acquire) {
+            interrupts::enable_and_hlt()
+        } else {
+            interrupts::enable()
         }
     }
 }
